310c5b45edce34d44882341c905f0aec0e23d958
Holger Könemann <office@holgerkoenemann.de>
2022-11-14 11:37:07 +0100

    1.1.1 update

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,7 @@ gulp.task('purgecss', () => {
     return gulp.src('public/css/theme.min.css')
         .pipe(purgecss({
             content: ['public/**/*.html'],
-            safelist: ['collapsed', 'collapse', 'active', 'show', 'collapsing' ]
+            safelist: ['collapsed', 'collapse', 'active', 'show', 'showing', 'collapsing', 'modal-open', 'modal-backdrop', 'offcanvas-backdrop', 'fade', 'start' ]
         }))
         .pipe(gulp.dest('public/css'))
 })
